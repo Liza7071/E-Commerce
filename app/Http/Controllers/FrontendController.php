d331e7b88915155f6ce2b2aea874332dbb5cb557
unknown <liza7071>
2025-07-14 07:52:42 -0700

    Update

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    public function index(){
+
+        $categories = Category::orderBy('name', 'asc')->with('subCategory')->get();
+        $hotProducts = Product::where('product_type', 'hot')->orderBy('id', 'desc')->get();
+        $newProducts = Product::where('product_type', 'new')->orderBy('id', 'desc')->get();
+        $regularProducts = Product::where('product_type', 'regular')->orderBy('id', 'desc')->get();
+        $discountProducts = Product::where('product_type', 'discount')->orderBy('id', 'desc')->get();
+
+        return view('frontend.index', compact('hotProducts', 'newProducts', 'regularProducts', 'discountProducts', 'categories'));
+    }
+
+    public function shopProducts ()
+    {
+        return view('frontend.shop');
+    }
+
+    public function returnProcess ()
+    {
+        return view('frontend.return-process');
+    }
+
+    public function productDetails ($slug)
+    {
+        $product = Product::where('slug', $slug)->with('color', 'size', 'galleryImage')->first();
+        $categories = Category::orderBy('name', 'asc')->get();
+        return view('frontend.product-details', compact('product', 'categories'));
+    }
+
+    public function typeProducts ($type)
+    {
+        return view('frontend.type-products', compact('type'));
+    }
+
+    public function viewCart ()
+    {
+        return view('frontend.view-cart');
+    }
+
+    public function checkOut ()
+    {
+        return view('frontend.checkout');
+    }
+
+    public function privacyPolicy ()
+    {
+        return view('frontend.privacy-policy');
+    }
+
+    public function termsCondition ()
+    {
+        return view('frontend.terms-conditions');
+    }
+
+    public function refundPolicy ()
+    {
+        return view('frontend.refund-policy');
+    }
+
+    public function paymentPolicy ()
+    {
+        return view('frontend.payment-policy');
+    }
+
+    public function aboutUs ()
+    {
+        return view('frontend.about-us');
+    }
+
+    public function contactUs ()
+    {
+        return view('frontend.contact-us');
+    }
+}
